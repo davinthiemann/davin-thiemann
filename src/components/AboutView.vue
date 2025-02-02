2eb1f50aa942e42dfd9d496b8e0e7cb26158f21d
Davin Thiemann <davin.thiemann@hdnet.de>
2025-02-02 23:08:19 +0100

    chore: Install bootstrap and scss

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <h1> About </h1>
+  <h1 class="test"> About </h1>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import router from "../routes.js";
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import '@/assets/styles.scss';
 
 createApp(App).use(router).mount('#app')
